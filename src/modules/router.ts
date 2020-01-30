1df14e64be98399338d3144d2ed9036763a6b642
Cameron Fleming <cameron@nevexo.space>
2020-01-30 23:02:35 +0000

    Reduce router to one class, import other classes

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -3,50 +3,51 @@
 
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
+import { Uplink }   from "./uplink";
+import { Response } from './response';
+import { State }    from '../interfaces/state';
 import * as config from '../config.json';
-
-class Handlers {
-    public default(req: any, res: any, next: any) {
-        // Called for every API call
-        res.set("Content-Type", "application/json")
-        res.set("X-Powered-By", "R.A.M")
-        next()
-    }
-
-    public aggregates(req: any, res: any, next: any) {
-        res.json({"yeet": "woo, api"})
-    }
-}
 
 export class Router {
     app: any;
-    handlers: any;
     logger: any;
 
     constructor(logger: any) {
         // Setup logger
-        this.logger = logger
+        this.logger = logger;
         // Setup handlers instance
-        this.handlers = new Handlers;
-        // Setup Express router
         this.app = express();
         // Enable CORS headers
         this.app.use(cors());
         // Enable default handler
-        this.app.use(this.handlers.default);
+        this.app.use((req: any, res: any, next: any) => {
+            this.default(req, res, next);
+        });
 
         // Add aggregates endpoint
-        this.app.get(`${config.api.base_endpoint}/aggregates`, this.handlers.aggregates)
+        this.app.get(`${config.api.base_endpoint}/aggregates`, (req: any, res: any) => {
+            this.aggregates(req, res);
+        })
 
         // Start API
         this.app.listen(config.api.port, (error: Error) => {
             if (error) {
-                this.logger.error("FATAL! Failed to start Express Server.")
-                this.logger.error(error)
+                this.logger.error("FATAL! Failed to start Express Server.");
+                this.logger.error(error);
             }
-            this.logger.info(`API Started successfully, now listening on port ${config.api.port}.`)
+            this.logger.info(`API Started successfully, now listening on port ${config.api.port}.`);
         })
     }
 
+    default(req: any, res: any, next: any) {
+        // Called for every API call
+        res.set("Content-Type", "application/json")
+        res.set("X-Powered-By", "R.A.M")
+        this.logger.verbose(`API Act: ${req.hostname} ${req.originalUrl}`)
+        next()
+    }
+
+    aggregates(req: any, res: any) {
+        res.json({"yeet": "woo, api"})
+    }
 }
