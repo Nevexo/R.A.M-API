ab7ef63caf2d70d6ce7fd2b0ac07fc06ff9d921e
Cameron Fleming <cameron@nevexo.space>
2020-01-30 22:51:36 +0000

    Add Winston logger

--- src/api.ts
+++ src/api.ts
@@ -5,4 +5,22 @@ import { Router }   from "./modules/router";
 import { Uplink }   from "./modules/uplink";
 import { Response } from './modules/response';
 import { State }    from './interfaces/state';
+import Winston from 'winston';
 
+const logger = Winston.createLogger({
+    level: 'info',
+    format: Winston.format.json(),
+    transports: [
+      //
+      // - Write all logs with level `error` and below to `error.log`
+      // - Write all logs with level `info` and below to `combined.log`
+      //
+      new Winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new Winston.transports.File({ filename: 'combined.log' }),
+      new Winston.transports.Console( {format: Winston.format.simple() })
+    ]
+});
+
+logger.info("Initalizing...")
+
+const router = new Router(logger);
